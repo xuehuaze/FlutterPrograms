d3c887db52dbd6cc9a02ab0244094c68bab4a5f1
GuHaijun <243297288@qq.com>
2019-06-24 17:07:42 +0800

    #modify 升级flutter版本到1.5.4.2

--- flutter_app/lib/network/api.dart
+++ flutter_app/lib/network/api.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 
 class Api {
-  static Uri specsList = Uri.parse("https://raw.githubusercontent.com/FlutterPrograms/Specs/master/specs/specs.json");
+  static Uri specsList = Uri.parse("https://raw.githubusercontent.com/FlutterPrograms/Specs/master/specs/flutter_v1.5.4.2/specs.json");
 }
